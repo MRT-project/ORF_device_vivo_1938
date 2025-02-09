7620a44e2cb3671411c17d71c6193b67afbf0fb6
UntuKemeng <jarbull87@gmail.com>
2025-02-09 14:19:59 +0700

    Little bug fix

--- fox_1938.mk
+++ fox_1938.mk
@@ -75,11 +75,13 @@ OF_WIPE_METADATA_AFTER_DATAFORMAT := 1
 FOX_USE_LZ4_BINARY := 1
 
 # Nano
-FOX_EXCLUDE_NANO_EDITOR := 1
 FOX_USE_NANO_EDITOR := 1
 
 # Magisk (Magisk does not work on vivo)
 FOX_DELETE_MAGISK_ADDON := 1
+
+# Use lzma on ramdisk compression
+OF_USE_LZMA_COMPRESSION := 1
 
 # Binaries
 FOX_USE_BASH_SHELL := 1
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,54 +18,9 @@
 # 	Please maintain this if you use this script or any part of it
 #
 
+oem="vivo"
 export TZ="Asia/Jakarta"
 export LC_ALL="C"
-export FOX_VERSION="R12.1"
-export FOX_BUILD_TYPE=$(date +%y.%m.%d)
+export FOX_BUILD_TYPE=$(date +%y.%m.%d)-$oem
 export ALLOW_MISSING_DEPENDENCIES=true
 export TW_DEFAULT_LANGUAGE="en"
-
-FDEVICE="1938"
-
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep \"$FDEVICE\")
-   if [ -n "$chkdev" ]; then
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep \"$FDEVICE\")
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
-}
-
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
-fi
-
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	export ALLOW_MISSING_DEPENDENCIES=true
-	export FOX_ENABLE_APP_MANAGER=1
-	export TARGET_DEVICE_ALT="1938"
-	export FOX_TARGET_DEVICES="1938"
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export FOX_USE_LZ4_BINARY=1
-	export FOX_USE_ZSTD_BINARY=1
-
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
-else
-	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
-		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
-	fi
-fi
-#
